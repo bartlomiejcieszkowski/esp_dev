01883b223573b79d2ba9380a087f10ff46c49f4b
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2019-11-18 19:16:04 +0100

    update template, add mine
    Signed-off-by: Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>

---
+++ burn_env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#author: Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
+
+if [[ -n "${BASH_SOURCE}" && ( "${BASH_SOURCE[0]}" == "${0}" ) ]]; then
+	echo "This script should be sourced not executed"
+	return 1
+fi
+
+export BURN_IDF_PATH=/home/bart/esp_dev/esp-idf
+#export BURN_UART=/dev/ttyUSB0
+#export BURN_UART="/dev/serial/by-path/pci-0000:00:14.0-usb-0:1.2.4:1.0-port0"
+#export BURN_UART="/dev/serial/by-path/pci-0000\:00\:14.0-usb-0\:1.2.4\:1.0-port0"
+export BURN_UART="/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A92117B3-if00-port0"
+export BURN_BAUD_RATE=115200
+export BURN_SSH_PORT=22
+export BURN_SSH_IP=192.168.1.201
+export BURN_SSH_USER=pi
+
